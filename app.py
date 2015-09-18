25c9c1872333b0e92633689028d53ee2316e43f0
CooperLuan <lannikcooper@gmail.com>
2015-09-18 09:39:43 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#!python3
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
+
+@app.route('/api/url/extract')
+def api_url_extract():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9801, debug=True)
