f34afab5f72d8ecd3cf626de0d06fc3c9c54a983
CooperLuan <lannikcooper@gmail.com>
2015-09-23 09:02:37 +0800

    changes

--- nyl.py
+++ nyl.py
@@ -1,6 +1,9 @@
 #!python2
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from models.url_model import URLModel
+from models.html_model import HTMLModel
 
 app = Flask('nyl')
 
@@ -15,9 +18,15 @@ def about():
     return render_template('index.html')
 
 
-@app.route('/api/url/extract')
+@app.route('/api/url/extract', methods=['POST'])
 def api_url_extract():
-    pass
+    url = request.form['url']
+    html = URLModel(url).get_html()
+    data = HTMLModel(html).extract_all()
+    return jsonify(**{
+        'code': 1,
+        'data': data,
+    })
 
 
 if __name__ == "__main__":
